f81c4d3e58fc07ee865b184285ab5ac83626391b
Luiz Arthur <121046321+EchoArthur@users.noreply.github.com>
2023-02-25 23:06:42 -0300

    Update dirbrute.sh

--- dirbrute.sh
+++ dirbrute.sh
@@ -5,7 +5,7 @@ echo "--------------------/////////////////////////////////---------------------
 echo "--------------------FORM OF USE: ./dirbrute.sh url -w wordlist------------------"
 echo "--------------------------------------------------------------------------------"
 echo "Searching for directors: "
-for palavras in $(cat $3)
+for words in $(cat $3)
 do
 
 if [ $2 != "-w" ]
@@ -13,15 +13,15 @@ then
 echo "USE -w TO CHOOSE A WORDLIST OR THE TOOL WILL NOT WORK!!"
 exit
 fi
-respost=$(curl -s -o /dev/null -w "%{http_code}" $1/$palavras/)
+respost=$(curl -s -o /dev/null -w "%{http_code}" $1/$words/)
 if [ $respost == "200" ]
 then
-echo " OPEN 200: $1/$palavras"
+echo " OPEN 200: $1/$words"
 fi
 
-if [ $resposta == "301" ]
+if [ $respost == "301" ]
 then
-echo "DIRECTORS FOUND FORBIDEN 301: $1/$palavras"
+echo "DIRECTORS FOUND FORBIDEN 301: $1/$words"
 fi
 done
 
